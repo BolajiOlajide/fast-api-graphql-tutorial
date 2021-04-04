5b426484b58ee2850715f444030a2920a3d700cd
Bolaji Olajide <cooproton@gmail.com>
2021-04-04 01:51:31 +0100

    feat: create graphql schema

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import graphene
 from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+
+from src.schema import Query
+
 
 app = FastAPI()
 
+app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query)))
 
 @app.get('/')
 def ping():
