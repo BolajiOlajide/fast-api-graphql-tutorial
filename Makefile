06a8cee3f4e8d0f868b3dd91c71f86189a01b1ac
Bolaji Olajide <cooproton@gmail.com>
2021-04-04 01:32:53 +0100

    feat: initialize orator

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
+NAME?=
+ORATOR_ERROR_MESSAGE="Please specify a name for the model to create. e.g make model_create NAME=User"
+
 dev:
 	uvicorn main:app --reload
 
 start:
 	uvicorn main:app
+
+model_create:
+ifeq ($(NAME),)
+	@ echo $(ORATOR_ERROR_MESSAGE)
+	exit 1
+else
+	orator make:model $(NAME) -m
+endif
---
+++ src/db.py
@@ -0,0 +1,20 @@
+from orator import DatabaseManager, Schema, Model
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+DATABASES = {
+    "postgres": {
+        "driver": "postgres",
+        "host": os.getenv("DB_HOST"),
+        "database": os.getenv("DB_NAME"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "prefix": "fagt_",
+        "port": 5432
+    }
+}
+
+db = DatabaseManager(DATABASES)
+schema = Schema(db)
+Model.set_connection_resolver(db)
