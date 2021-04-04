67e89839022bd737413fadf212a79f0319e58313
Bolaji Olajide <cooproton@gmail.com>
2021-04-04 02:46:42 +0100

    feat: setup test directory

---
+++ test/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from orator import DatabaseManager, Model, Schema
+from orator.migrations import DatabaseMigrationRepository, Migrator
+
+
+@pytest.fixture(autouse=True)
+def setup_database():
+    DATABASES = {
+        "sqlite": {
+            "driver": "sqlite",
+            "database": "test.db"
+        }
+    }
+
+    db = DatabaseManager(DATABASES)
+    Schema(db)
+
+    Model.set_connection_resolver(db)
+
+    repository = DatabaseMigrationRepository(db, "migrations")
+    migrator = Migrator(repository, db)
+
+    if not repository.repository_exists():
+        repository.create_repository()
+
+    migrator.reset("migrations")
+    migrator.run("migrations")
